2702f7b89f557c66308dad19f146469524688115
Kilian Nagel <nagelkilian05@gmail.com>
2022-02-24 16:58:07 +0100

    code cleaned + navbar animation

--- js/index.js
+++ js/index.js
@@ -5,14 +5,14 @@
 function init_listeners(){
     let mobile_navbar = document.querySelector('.mobile-navbar');
     let navbar_quit_btn = document.querySelector('.mobile-navbar__quit-btn');
-    let navbar_show_btn = document.querySelector('.navigation-icon');
+    let navbar_show_btn = document.querySelector('.home-hero__navigation-icon');
+
 
     navbar_show_btn.addEventListener('click',()=>{
-        mobile_navbar.style.display = "block";
-        console.log('wew');
+        mobile_navbar.classList.add('open-nav');
     })
     navbar_quit_btn.addEventListener('click',()=>{
-        mobile_navbar.style.display = "none";
+        mobile_navbar.classList.remove('open-nav')
     })
 }
 
